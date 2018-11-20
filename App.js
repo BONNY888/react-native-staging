25b6293da4cedfac868610a551af92f1d2b99ad3
andyka1714 <andyka1714@gmail.com>
2018-11-20 16:42:36 +0800

    install webview invoke, and add onmessage method to invoke with web

--- App.js
+++ App.js
@@ -1,39 +1,76 @@
 import React from 'react'
-import { StyleSheet, WebView, View, Text, Button, Share } from 'react-native'
+import { View, Button, alert, Text, StyleSheet, Share, WebView } from 'react-native'
+import createInvoke from 'react-native-webview-invoke/native'
 
 export default class App extends React.Component {
-  testShare () {
-    Share.share(
-      {
-        url: 'https://www.ptt.cc/bbs/Soft_Job/M.1534771650.A.5D5.html'
-      })
-      .then(result => console.log(result))
-      .catch(errorMsg => console.log(errorMsg))
+  constructor(props) {
+    super(props);
+    this.state = { 
+      message: 'hahahaha'
+    };
+    
   }
+  // webInitialize = () => {
+    
+  // }
+  webWannaGet = () => {
+    return 'haha!!'
+  }
+  webWannaSet = (data) => {
+    this.setState({ message: data })
+  }
+  
+  invoke = createInvoke(() => this.webview)
+  componentDidMount() {
+    this.invoke
+      // .define('init', this.webInitialize)
+      .define('get', this.webWannaGet)
+      .define('set', this.webWannaSet)
+  }
+  webview: WebView
 
-  render () {
-    return (
-      <View
-        style={{
-          flexDirection: 'row',
-          flex: 1
-        }}>
-        <WebView source={{uri: 'https://staging.bonnylive.biz/'}} style={styles.container} />
+  render() {
+    const patchPostMessageFunction = function() {
+        var originalPostMessage = window.postMessage;
+    
+        var patchedPostMessage = function(message, targetOrigin, transfer) {
+            originalPostMessage(message, targetOrigin, transfer);
+        };
+    
+        patchedPostMessage.toString = function() {
+            return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
+        };
+    
+        window.postMessage = patchedPostMessage;
+    };
+    
+    const patchPostMessageJsCode = '(' + String(patchPostMessageFunction) + ')();';
+      return (
         <View
           style={{
-            position: 'absolute', left: 0, right: 0, bottom: 50, justifyContent: 'flex-end', alignItems: 'center'
+            flex: 1
           }}>
-        >
-          <Button
-            onPress={this.testShare.bind(this)}
-            title='Share'
-            color='#fff'
-          />
+          {/* <View
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+          <Text>{this.state.message}</Text>
+          </View> */}
+          <WebView
+            injectedJavaScript={patchPostMessageJsCode}
+            ref={w => this.webview = w}
+            // source={require('./index.html')}
+            source={{uri: 'https://ee9c318d.ngrok.io'}}
+            onMessage={this.invoke.listener}
+            style={styles.container} />
         </View>
-      </View>
-    )
+      )
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -41,5 +78,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 })
